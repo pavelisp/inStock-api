ff268e9a8e9f97f01acfbdf03f84a70c47e835b0
Pavel Ispravnikov <isp.pavel@gmail.com>
2022-03-16 12:19:06 -0400

    added warehouses endpoint to routes and inside server.js

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+
 dotenv.config()
+
 const inventoryList = require('./routes/inventory');
+const warehouseList = require('./routes/warehouses');
 
 
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+
 app.use('/inventory', inventoryList);
+app.use('/warehouses', warehouseList);
 
 app.listen(SERVER_PORT, () =>{
     console.log(`Server is listening on port ${SERVER_PORT}`)
