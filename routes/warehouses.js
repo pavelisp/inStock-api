2457226a0af2214734ae40e315c2af05d396cd5d
natanael <nathan0002@outlook.es>
2022-03-17 20:21:41 -0400

    Created the endpoint to get a warehouse with its detail and inventory

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,93 +1,111 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
 let warehouses = {};
 
 // Read warehouses function, use readWarehouses() to update the list
 
-function readWarehouses(){
-    let warehouseData = fs.readFileSync("./data/warehouses.json")
-    const parsedWarehouses = JSON.parse(warehouseData);
-    return parsedWarehouses
+function readWarehouses() {
+  let warehouseData = fs.readFileSync("./data/warehouses.json");
+  const parsedWarehouses = JSON.parse(warehouseData);
+  return parsedWarehouses;
 }
 
-function writeWarehouses(data){
-    const stringifiedData = JSON.stringify(data);
-    fs.writeFileSync('./data/warehouses.json', stringifiedData);
+function writeWarehouses(data) {
+  const stringifiedData = JSON.stringify(data);
+  fs.writeFileSync("./data/warehouses.json", stringifiedData);
 }
 
+function getOneWarehouse(warehouseId) {
+  let warehouse = fs.readFileSync("./data/warehouses.json");
+  let inventories = fs.readFileSync("./data/inventories.json");
+  const parsedWarehouses = JSON.parse(warehouse);
+  const parsedInventories = JSON.parse(inventories);
+  let getOne = [];
+
+  let oneInventories = parsedInventories.find(
+    (inventories) => warehouseId === inventories.warehouseID
+  );
+
+  let oneWareHouse = parsedWarehouses.find(
+    (warehouse) => warehouseId === warehouse.id
+  );
+
+  getOne.push(oneWareHouse, oneInventories);
+
+  return getOne;
+}
 // Get all the warehouses at localhost:8080/warehouses/
 
-router.get('/', (req, res)=> {
+router.get("/", (req, res) => {
   readWarehouses();
   res.status(200).send(readWarehouses());
-})
+});
 
-router.post('/addWarehouse', (req,res) => {
-    const warehouses = readWarehouses();
-    const {name,
-        address,
-        city,
-        country,
-        contact,
-        position,
-        phone,
-        email
-        } = req.body; 
-        
-        if (!name){
-            return res.status(400).json({
-                message: "Warehouse name is REQUIRED!"
-            })
-        }
-        if (!address){
-                return res.status(400).json({
-                    message: "Warehouse address is REQUIRED!"
-                }) 
-        }
-        if (!city){
-                return res.status(400).json({
-                     message: "Warehouse city is REQUIRED!"
-                    }) 
-        }                    
-        if (!country){
-                return res.status(400).json({
-                message: "Warehouse country is REQUIRED!"
-                        })
-        }
-        if (!contact){
-            return res.status(400).json({
-                message: "Contact Name is REQUIRED!"
-            }) 
-        }
-            if (!position){
-            return res.status(400).json({
-                message: "Contact Position is REQUIRED!"
-            })
-        }
-            if (!phone){
-            return res.status(400).json({
-                message: "Phone Number is REQUIRED!"
-            })
-            }
-        if (!email){
-            return res.status(400).json({
-                message: "Email is REQUIRED!"
-            });
-        }
-            const newWarehouse = {name,
-                address,
-                city,
-                country,
-                contact,
-                position,
-                phone,
-                email,
-            };
-            warehouses.push(newWarehouse)
-            writeWarehouses(warehouses)
-            res.status(201).json(newWarehouse)
-        })
-        
+router.get("/:warehouseId", (req, res) => {
+  getOneWarehouse();
+  res.status(200).json(getOneWarehouse(req.params.warehouseId));
+});
+
+router.post("/addWarehouse", (req, res) => {
+  const warehouses = readWarehouses();
+  const { name, address, city, country, contact, position, phone, email } =
+    req.body;
+
+  if (!name) {
+    return res.status(400).json({
+      message: "Warehouse name is REQUIRED!",
+    });
+  }
+  if (!address) {
+    return res.status(400).json({
+      message: "Warehouse address is REQUIRED!",
+    });
+  }
+  if (!city) {
+    return res.status(400).json({
+      message: "Warehouse city is REQUIRED!",
+    });
+  }
+  if (!country) {
+    return res.status(400).json({
+      message: "Warehouse country is REQUIRED!",
+    });
+  }
+  if (!contact) {
+    return res.status(400).json({
+      message: "Contact Name is REQUIRED!",
+    });
+  }
+  if (!position) {
+    return res.status(400).json({
+      message: "Contact Position is REQUIRED!",
+    });
+  }
+  if (!phone) {
+    return res.status(400).json({
+      message: "Phone Number is REQUIRED!",
+    });
+  }
+  if (!email) {
+    return res.status(400).json({
+      message: "Email is REQUIRED!",
+    });
+  }
+  const newWarehouse = {
+    name,
+    address,
+    city,
+    country,
+    contact,
+    position,
+    phone,
+    email,
+  };
+  warehouses.push(newWarehouse);
+  writeWarehouses(warehouses);
+  res.status(201).json(newWarehouse);
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-dotenv.config()
+dotenv.config();
 
-const inventoryList = require('./routes/inventory');
-const warehouseList = require('./routes/warehouses');
-
+const inventoryList = require("./routes/inventory");
+const warehouseList = require("./routes/warehouses");
+const getOneWarehouse = require("./routes/warehouses");
 
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use('/inventory', inventoryList);
-app.use('/warehouses', warehouseList);
+app.use("/inventory", inventoryList);
+app.use("/warehouses", warehouseList);
+app.use("/warehouse", getOneWarehouse);
 
-app.listen(SERVER_PORT, () =>{
-    console.log(`Server is listening on port ${SERVER_PORT}`)
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is listening on port ${SERVER_PORT}`);
 });
-
-
-
-
-
-
